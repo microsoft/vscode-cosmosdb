4c6fd0155cc22ab99fae2af4c309edad2e7d4c38
Eric Jizba <erijiz@microsoft.com>
2017-09-15 10:39:54 -0700

    Make telemetry consistent with appservice Repo

--- src/util.ts
+++ src/util.ts
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { reporter } from './telemetry';
+
 export interface IDisposable {
 	dispose(): void;
 }
@@ -14,4 +16,32 @@ export function dispose<T extends IDisposable>(disposables: T[]): T[] {
 
 export function toDisposable(dispose: () => void): IDisposable {
 	return { dispose };
+}
+
+// Telemetry for the extension
+export function sendTelemetry(eventName: string, properties?: { [key: string]: string; }, measures?: { [key: string]: number; }) {
+	if (reporter) {
+		reporter.sendTelemetryEvent(eventName, properties, measures);
+	}
+}
+
+export function errToString(error: any): string {
+	if (error === null || error === undefined) {
+		return '';
+	}
+
+	if (error instanceof Error) {
+		return JSON.stringify({
+			'Error': error.constructor.name,
+			'Message': error.message
+		});
+	}
+
+	if (typeof (error) === 'object') {
+		return JSON.stringify({
+			'object': error.constructor.name
+		});
+	}
+
+	return error.toString();
 }

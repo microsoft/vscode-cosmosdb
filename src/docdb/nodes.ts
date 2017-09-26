3f157676648387f16f0f32f3983b43175c4e9ae1
Prashanth Srinivasan <prsri@microsoft.com>
2017-09-26 14:31:52 -0700

    DocDBNodes more scratchwork

--- src/docdb/nodes.ts
+++ src/docdb/nodes.ts
@@ -8,6 +8,7 @@ import { ResourceManagementClient } from 'azure-arm-resource';
 import docDBModels = require("azure-arm-documentdb/lib/models");
 import DocumentdbManagementClient = require("azure-arm-documentdb");
 import {MongoDatabaseNode} from '../mongo/nodes';
+var DocumentDBConnectionClient = require("documentdb").DocumentClient; 
 
 export interface DocDBCommand {
 	range: vscode.Range;
@@ -19,6 +20,7 @@ export interface DocDBCommand {
 
 export interface IDocDBServer extends INode {
 	getPrimaryMasterKey(): Promise<string>;
+	getEndpoint(): Promise<string>;
 }
 
 export class DocDBServerNode implements IDocDBServer {
@@ -27,7 +29,7 @@ export class DocDBServerNode implements IDocDBServer {
 
 	readonly collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
 
-	constructor(private readonly _primaryMasterKey: string, readonly id: string) {
+	constructor(private readonly _primaryMasterKey: string, readonly id: string, private readonly _endpoint: string) {
 		this.label = id;
 	}
 
@@ -42,9 +44,14 @@ export class DocDBServerNode implements IDocDBServer {
 		return Promise.resolve(this._primaryMasterKey);
 	}
 
+	getEndpoint(): Promise<string>{
+		return Promise.resolve(this._endpoint);
+	}
+
     getChildren(): Promise<INode[]> {
-		return new Promise<DocDBServerNode[]>;
-		//return DocDBServerNode.getDocDBDatabaseNodes(this._primaryMasterKey, this);
+		//return new Promise<DocDBServerNode[]>;
+		let client = new DocumentDBConnectionClient(this.getEndpoint(), this.getPrimaryMasterKey());
+		return this.getDocDBDatabaseNodes(client, this);
 	}
 
 	async listDatabases(client): Promise<any[]>{
@@ -63,7 +70,7 @@ export class DocDBServerNode implements IDocDBServer {
 		return await toArrayPromise;
 	}
         
-    async getDocDBDatabaseNodes(client, thisArg): Promise<INode[]> {
+    async getDocDBDatabaseNodes(client, DocDBServerNodeInstance): Promise<INode[]> {
         let databases;
         try{
             databases = await this.listDatabases(client);
@@ -71,7 +78,7 @@ export class DocDBServerNode implements IDocDBServer {
             databases = [];
             vscode.window.showErrorMessage(err.code + ": " + JSON.parse(err.body).message);
         }
-        return databases.map(database => new MongoDatabaseNode(database.id, <string>thisArg._databaseAccount.name));
+        return databases.map(database => new DocDBDatabaseNode(database.id, DocDBServerNodeInstance));
         //return new Promise<DocDBServerNode[]>;
 	}
 	
@@ -96,8 +103,46 @@ export class DocDBDatabaseNode implements INode {
 
 	readonly collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
 
+	getDbLink(): string {
+		return 'dbs/' + this.id;
+	}
 
+	getChildren(): Promise<INode[]> {
+		return this.getCollections(); 
+	}
 
+	async getCollections(): Promise<INode[]> {
+		let dbLink: string = this.getDbLink();
+		let collections;
+		let parentNode = this; 
+		let client = new DocumentDBConnectionClient(this.server.getEndpoint(), this.server.getPrimaryMasterKey());
+        try{
+            collections = await this.listCollections(dbLink, client);
+        }catch(err) {
+            collections = [];
+            vscode.window.showErrorMessage(err.code + ": " + JSON.parse(err.body).message);
+        }
+        return collections.map(collection => new DocDBCollectionNode(collection.id, parentNode));
+	}
+
+	async listCollections(databaseLink, client): Promise<any> {
+		let collections = await client.readCollections(databaseLink)
+		let toArrayPromise = new Promise<any[]>((resolve, reject) => {
+			collections.toArray(function (err, cols: Array<Object>) {
+				if (err) {
+					console.log("Error!" + JSON.stringify(err));
+					reject(err);
+				} 
+				else {
+					console.log("Cols: "+cols.length);
+					console.log(JSON.stringify(cols));            
+					resolve(cols);
+				}
+			});
+	
+		});
+		return await toArrayPromise; 
+	}
 
 
 }
@@ -105,15 +150,15 @@ export class DocDBDatabaseNode implements INode {
 
 export class DocDBCollectionNode implements INode {
 	
-		constructor(private collection, readonly db: DocDBDatabaseNode) {
+		constructor(private readonly identifier:string, readonly db: DocDBDatabaseNode) { 
 		}
 	
 		get id(): string {
-			return this.collection.collectionName;
+			return this.id;
 		}
 	
 		get label(): string {
-			return this.collection.collectionName;
+			return this.id;
 		}
 	
 		get iconPath(): any {

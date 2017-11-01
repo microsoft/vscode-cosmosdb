70add1f433472b49a973288de07c184bdd3f74c4
Eric Jizba <erijiz@microsoft.com>
2017-11-01 19:44:01 +0000

    Show error when copying connection string on linux without xclip installed (#122)

---
+++ src/utils/cp.ts
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as cp from 'child_process';
+
+export async function commandSucceeds(command: string, ...args: string[]): Promise<boolean> {
+    return await new Promise<boolean>(resolve => {
+        cp.spawn(command, args)
+            .on('error', err => resolve(false))
+            .on('exit', code => resolve(code === 0));
+    });
+}

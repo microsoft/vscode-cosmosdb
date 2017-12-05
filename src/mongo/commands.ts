4567ee8229572b36a5b6bdfb18bf9cf25f72b68e
Prashanth <prsri@microsoft.com>
2017-12-05 13:27:58 -0800

    Fix #253. Change signature and callsite of showDocument

--- src/mongo/commands.ts
+++ src/mongo/commands.ts
@@ -34,7 +34,7 @@ export class MongoCommands {
 				const db = await database.getDb();
 				let node = new MongoCollectionNode(db.collection(command.collection), database, command.arguments);
 				await node.getChildren();
-				await editor.showDocument(node, extensionPath, 'cosmos-result.json');
+				await editor.showDocument(node, 'cosmos-result.json');
 				return command;
 			}
 			const result = await database.executeCommand(command);
@@ -46,7 +46,7 @@ export class MongoCommands {
 					id = null;
 				}
 				let node = new MongoDocumentNode(id, null, parsed);
-				await editor.showDocument(node, extensionPath, 'cosmos-result.json');
+				await editor.showDocument(node, 'cosmos-result.json');
 			}
 			else {
 				await util.showNewFile(result, extensionPath, 'result', '.json', activeEditor.viewColumn + 1);

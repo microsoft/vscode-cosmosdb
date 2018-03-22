e6410ac5f456cccdfeb43df3e4a61020d785f276
Eric Jizba <erijiz@microsoft.com>
2018-03-22 21:25:12 +0000

    Refactor createCosmosDBAccount to use shared wizard (#412)
    In addition to reducing the amount of copied code, this adds support for the following:
    
    1. Persisting recently used selections
    1. Generating a related name for the resource group
    1. Consistent use of ignoreFocusOut and UserCancelledError by default

---
+++ src/tree/CosmosDBAccountWizard/CosmosDBAccountNameStep.ts
@@ -0,0 +1,47 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import CosmosDBManagementClient = require("azure-arm-cosmosdb");
+import { IAzureUserInput, AzureNameStep, ResourceGroupStep, resourceGroupNamingRules } from 'vscode-azureextensionui';
+import { ICosmosDBWizardContext } from './ICosmosDBWizardContext';
+
+export class CosmosDBAccountNameStep extends AzureNameStep<ICosmosDBWizardContext> {
+    protected async isRelatedNameAvailable(wizardContext: ICosmosDBWizardContext, name: string): Promise<boolean> {
+        return await ResourceGroupStep.isNameAvailable(wizardContext, name);
+    }
+
+    public async prompt(wizardContext: ICosmosDBWizardContext, ui: IAzureUserInput): Promise<ICosmosDBWizardContext> {
+        const client = new CosmosDBManagementClient(wizardContext.credentials, wizardContext.subscription.subscriptionId);
+        wizardContext.accountName = (await ui.showInputBox({
+            placeHolder: "Account name",
+            prompt: "Provide a Cosmos DB account name",
+            validateInput: (name: string) => validateCosmosDBAccountName(name, client)
+        })).trim();
+
+        wizardContext.relatedNameTask = this.generateRelatedName(wizardContext, wizardContext.accountName, resourceGroupNamingRules);
+
+        return wizardContext;
+    }
+
+    public async execute(wizardContext: ICosmosDBWizardContext): Promise<ICosmosDBWizardContext> {
+        return wizardContext;
+    }
+}
+
+async function validateCosmosDBAccountName(name: string, client: CosmosDBManagementClient): Promise<string | undefined> {
+    name = name ? name.trim() : '';
+
+    const min = 3;
+    const max = 31;
+    if (name.length < min || name.length > max) {
+        return `The name must be between ${min} and ${max} characters.`;
+    } else if (name.match(/[^a-z0-9-]/)) {
+        return "The name can only contain lowercase letters, numbers, and the '-' character.";
+    } else if (await client.databaseAccounts.checkNameExists(name)) {
+        return `Account name "${name}" is not available.`
+    } else {
+        return undefined;
+    }
+}

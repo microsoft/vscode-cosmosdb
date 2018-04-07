45ff8f1e17f0e8eebcf1e35f667a6b5ebd2629a0
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2018-04-06 17:18:39 -0700

    Use timeout on account name validation, fixes #494 (#501)
    * Use timeout on account name validation, fixes #494
    
    * fix
    
    * Add unit tests and make some fixes
    
    * fixes

---
+++ src/utils/timeout.ts
@@ -0,0 +1,55 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+const timedOutMessage = "Execution timed out";
+
+/**
+ * Returns the result of awaiting a specified action. Rejects if the action throws. Returns timeoutValue if a time-out occurs.
+ */
+export async function valueOnTimeout<T>(timeoutMs: number, timeoutValue: T, action: () => Promise<T> | T) {
+    try {
+        return await rejectOnTimeout(timeoutMs, action);
+    } catch (err) {
+        let error = <{ message?: string }>err;
+        if (error && error.message === timedOutMessage) {
+            return timeoutValue;
+        }
+
+        throw err;
+    }
+}
+
+/**
+ * Returns the result of awaiting a specified action. Rejects if the action throws or if the time-out occurs.
+ */
+export async function rejectOnTimeout<T>(timeoutMs: number, action: () => Promise<T> | T) {
+    return await new Promise<T>(async (resolve, reject) => {
+        let timer: NodeJS.Timer = setTimeout(
+            () => {
+                timer = null;
+                reject(new Error(timedOutMessage));
+            },
+            timeoutMs);
+
+        let value: T;
+        let error;
+
+        try {
+            value = await action();
+        } catch (err) {
+            error = err;
+        }
+
+        if (timer) {
+            clearTimeout(timer);
+
+            if (error) {
+                reject(error);
+            } else {
+                resolve(value);
+            }
+        }
+    });
+}
---
+++ test/timeout.test.ts
@@ -0,0 +1,127 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+// The module 'assert' provides assertion methods from node
+import * as assert from 'assert';
+import { rejectOnTimeout, valueOnTimeout } from '../src/utils/timeout';
+
+suite("timeout Tests", () => {
+    suite("rejectOnTimeout", () => {
+
+        test("executes synchronously", async () => {
+            let executed: boolean;
+
+            await rejectOnTimeout(1, () => {
+                executed = true;
+            });
+
+            assert.equal(executed, true);
+        });
+
+        test("executes synchronously in promise", async () => {
+            let executed = false;
+
+            await rejectOnTimeout(1, () => {
+                return new Promise((resolve, reject) => {
+                    executed = true;
+                    resolve();
+                });
+            })
+
+            assert.equal(executed, true);
+        });
+
+        test("executes asynchnously before time-out", async () => {
+            let executed = false;
+
+            await rejectOnTimeout(1000, () => {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        executed = true;
+                        resolve();
+                    }, 1);
+                });
+            })
+            assert.equal(executed, true);
+        });
+
+        test("timed out", async () => {
+            let executed = false;
+
+            try {
+                await rejectOnTimeout(1, async () => {
+                    await new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            executed = true;
+                            resolve();
+                        }, 1000);
+                    });
+                });
+
+                assert.fail(null, null, "Expected exception");
+            } catch (error) {
+            }
+
+            assert.equal(executed, false);
+        });
+
+        test("throws before time-out", async () => {
+            let executed = false;
+            let error: Error;
+
+            try {
+                await rejectOnTimeout(1000, async () => {
+                    await new Promise((resolve, reject) => {
+                        throw new Error("I threw up");
+                    });
+                })
+            } catch (err) {
+                error = err;
+            }
+
+            assert.equal(executed, false);
+            assert.equal(error.message, "I threw up");
+        });
+    });
+
+    suite("valueOnTimeout", () => {
+
+        test("executed", async () => {
+            let value = await valueOnTimeout(1000, 123, async () => {
+                return await new Promise<number>((resolve, reject) => {
+                    setTimeout(() => { resolve(-123); }, 1);
+                });
+            });
+
+            assert.equal(value, -123);
+        });
+
+        test("timed out", async () => {
+            let value = await valueOnTimeout(1, 123, async () => {
+                return await new Promise<number>((resolve, reject) => {
+                    setTimeout(() => { resolve(-123); }, 1000);
+                });
+            });
+
+            assert.equal(value, 123);
+        });
+
+        test("reject", async () => {
+            let error;
+            try {
+                await valueOnTimeout(1000, 123, async () => {
+                    return await new Promise<number>((resolve, reject) => {
+                        setTimeout(() => { reject(new Error("rejected")); }, 1);
+                    });
+                });
+            } catch (err) {
+                error = err;
+            }
+
+            assert.equal(error && error.message, "rejected");
+        });
+    });
+
+});

1653ace5ae199b2299f5ec62627ada27887a3677
Prashanth <30420973+PrashanthCorp@users.noreply.github.com>
2018-04-11 11:21:48 -0700

    Implement 'no Unused Parameters' typescript compilation rule (#518)
    * Implement rule
    
    * Based on Stephen's feedback.

--- src/utils/cp.ts
+++ src/utils/cp.ts
@@ -8,7 +8,7 @@ import * as cp from 'child_process';
 export async function commandSucceeds(command: string, ...args: string[]): Promise<boolean> {
     return await new Promise<boolean>(resolve => {
         cp.spawn(command, args)
-            .on('error', err => resolve(false))
+            .on('error', _error => resolve(false))
             .on('exit', code => resolve(code === 0));
     });
 }

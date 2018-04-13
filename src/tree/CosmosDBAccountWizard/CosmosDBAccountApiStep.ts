f79c3bd67f9d2720248ed2ffa135b42b11af9f53
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2018-04-13 12:11:18 -0700

    Update experience names and order to match portal  (#524)
    * Update experience names and order to match portal
    
    * fix
    
    * Refactor

--- src/tree/CosmosDBAccountWizard/CosmosDBAccountApiStep.ts
+++ src/tree/CosmosDBAccountWizard/CosmosDBAccountApiStep.ts
@@ -5,23 +5,17 @@
 
 import { AzureWizardStep, IAzureUserInput, IAzureQuickPickItem } from 'vscode-azureextensionui';
 import { ICosmosDBWizardContext } from './ICosmosDBWizardContext';
-import { Experience, DBAccountKind } from '../../constants';
+import { Experience, getExperienceQuickPicks } from '../../experiences';
 
 export class CosmosDBAccountApiStep extends AzureWizardStep<ICosmosDBWizardContext> {
     public async prompt(wizardContext: ICosmosDBWizardContext, ui: IAzureUserInput): Promise<ICosmosDBWizardContext> {
-        const picks: IAzureQuickPickItem<string>[] = [
-            { label: Experience.MongoDB, description: '', data: DBAccountKind.MongoDB },
-            { label: Experience.DocumentDB, description: '', data: DBAccountKind.GlobalDocumentDB },
-            { label: Experience.Graph, description: '', data: DBAccountKind.GlobalDocumentDB },
-            { label: Experience.Table, description: '', data: DBAccountKind.GlobalDocumentDB }
-        ];
+        const picks: IAzureQuickPickItem<Experience>[] = getExperienceQuickPicks();
 
-        const result: IAzureQuickPickItem<string> = await ui.showQuickPick(picks, {
+        const result: IAzureQuickPickItem<Experience> = await ui.showQuickPick(picks, {
             placeHolder: "Select an API for your Cosmos DB account...",
         });
 
-        wizardContext.defaultExperience = <Experience>result.label;
-        wizardContext.kind = <DBAccountKind>result.data;
+        wizardContext.defaultExperience = result.data;
 
         return wizardContext;
     }

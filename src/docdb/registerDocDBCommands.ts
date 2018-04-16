57f954cb59e8a2e21a9170446486eb6f991a4509
Prashanth <prsri@microsoft.com>
2018-04-16 13:29:45 -0700

    Initial Commit: revert PR 479 https://github.com/Microsoft/vscode-cosmosdb/pull/479

--- src/docdb/registerDocDBCommands.ts
+++ src/docdb/registerDocDBCommands.ts
@@ -8,6 +8,7 @@ import { DocDBDatabaseTreeItem } from "./tree/DocDBDatabaseTreeItem";
 import { DocDBAccountTreeItem } from "./tree/DocDBAccountTreeItem";
 import { DocDBCollectionTreeItem } from "./tree/DocDBCollectionTreeItem";
 import { DocDBDocumentTreeItem } from "./tree/DocDBDocumentTreeItem";
+import { DocDBDocumentsTreeItem } from "./tree/DocDBDocumentsTreeItem";
 import { DocDBStoredProcedureTreeItem } from "./tree/DocDBStoredProcedureTreeItem";
 
 export function registerDocDBCommands(actionHandler: AzureActionHandler, tree: AzureTreeDataProvider): void {
@@ -27,8 +28,8 @@ export function registerDocDBCommands(actionHandler: AzureActionHandler, tree: A
     actionHandler.registerCommand('cosmosDB.createDocDBDocument', async (node?: IAzureParentNode) => {
         if (!node) {
             // #region Temporary changes to remove Documents node until viewing/editor stored procedures is implemented
-            // node = <IAzureParentNode>await tree.showNodePicker(DocDBDocumentsTreeItem.contextValue);
-            node = <IAzureParentNode>await tree.showNodePicker(DocDBCollectionTreeItem.contextValue);
+            node = <IAzureParentNode>await tree.showNodePicker(DocDBDocumentsTreeItem.contextValue);
+            //node = <IAzureParentNode>await tree.showNodePicker(DocDBCollectionTreeItem.contextValue);
             // #endregion
         }
         await node.createChild();

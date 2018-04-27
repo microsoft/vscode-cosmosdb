6d33a90b6051f9ff32993f6618376b083403aa88
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2018-04-27 15:24:08 -0700

    Honor vscode http.proxyStrictSSL setting (#568)
    * Open newly-created docs in editor
    
    * Honor vscode http.proxyStrictSSL setting

--- src/docdb/getDocumentClient.ts
+++ src/docdb/getDocumentClient.ts
@@ -3,13 +3,18 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as vscode from "vscode";
 import { DocumentClient } from "documentdb";
 import * as DocDBLib from 'documentdb/lib';
+import { ext } from "../extensionVariables";
 
 export function getDocumentClient(documentEndpoint: string, masterKey: string, isEmulator: boolean): DocumentClient {
     const documentBase = DocDBLib.DocumentBase;
     var connectionPolicy = new documentBase.ConnectionPolicy();
-    connectionPolicy.DisableSSLVerification = isEmulator;
+
+    let vscodeStrictSSL: boolean | undefined = vscode.workspace.getConfiguration().get<boolean>(ext.settingsKeys.vsCode.proxyStrictSSL);
+    let strictSSL = !isEmulator && vscodeStrictSSL;
+    connectionPolicy.DisableSSLVerification = !strictSSL;
     const client = new DocumentClient(documentEndpoint, { masterKey: masterKey }, connectionPolicy);
     return client;
 }

61e1fc2abb094a61e8489c71e2b0bb7221b87a08
Prashanth <30420973+PrashanthCorp@users.noreply.github.com>
2018-05-08 10:55:45 -0700

    Implement tslint: no-var-keyword. Replace var -> let wherever applicable. (#617)
    * Implement tslint: no-var-keyword. var -> let wherever applicable.
    
    * no-var-keyword in the rest of code base

--- src/docdb/getDocumentClient.ts
+++ src/docdb/getDocumentClient.ts
@@ -10,7 +10,7 @@ import { ext } from "../extensionVariables";
 
 export function getDocumentClient(documentEndpoint: string, masterKey: string, isEmulator: boolean): DocumentClient {
     const documentBase = DocDBLib.DocumentBase;
-    var connectionPolicy = new documentBase.ConnectionPolicy();
+    let connectionPolicy = new documentBase.ConnectionPolicy();
 
     let vscodeStrictSSL: boolean | undefined = vscode.workspace.getConfiguration().get<boolean>(ext.settingsKeys.vsCode.proxyStrictSSL);
     let strictSSL = !isEmulator && vscodeStrictSSL;

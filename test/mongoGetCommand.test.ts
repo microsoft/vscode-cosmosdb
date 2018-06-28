79829311a0b9272f8b49684ebbf687b972090931
Prashanth <prsri@microsoft.com>
2018-06-27 19:32:18 -0700

    1. Allow collection names to have dots. 2. Modify some tests to check for strings in double quotes, single quotes and no quotes 3. Add test cases from user-filed issues.

--- test/mongoGetCommand.test.ts
+++ test/mongoGetCommand.test.ts
@@ -70,7 +70,7 @@ function testParse(
     testCore(spaceText);
 }
 
-/*
+
 function wrapInQuotes(word: string, numQuotes: number) { //0 to do nothing, 1 for single quotes, 2 for double quotes
     let result: string;
     if (numQuotes === 1) {
@@ -82,7 +82,6 @@ function wrapInQuotes(word: string, numQuotes: number) { //0 to do nothing, 1 fo
     }
     return result;
 }
-*/
 
 suite("scrapbook parsing Tests", () => {
     test("find", () => {
@@ -107,18 +106,22 @@ suite("scrapbook parsing Tests", () => {
 
     test("second of two commands, Mac/Linux", () => {
         let line1 = "db.find()";
-        let line2 = "db.insertOne({'a':'b'})";
-        let text = `${line1}\n${line2}`;
-        let command = getCommandFromTextAtLocation(text, new Position(2, 0));
-        assert.equal(command.text, line2);
+        for (let i in [0, 1, 2]) {
+            let line2 = `db.insertOne({${wrapInQuotes("a", +i)}:'b'})`;
+            let text = `${line1}\n${line2}`;
+            let command = getCommandFromTextAtLocation(text, new Position(2, 0));
+            assert.equal(command.text, line2);
+        }
     });
 
     test("second of two commands, Mac/Linux, semicolon", () => {
         let line1 = "db.find();";
-        let line2 = "db.insertOne({'a':'b'})";
-        let text = `${line1}\n${line2}`;
-        let command = getCommandFromTextAtLocation(text, new Position(2, 0));
-        assert.equal(command.text, line2);
+        for (let i in [0, 1, 2]) {
+            let line2 = `db.insertOne({${wrapInQuotes("a", +i)}:'b'})`;
+            let text = `${line1}\n${line2}`;
+            let command = getCommandFromTextAtLocation(text, new Position(2, 0));
+            assert.equal(command.text, line2);
+        }
     });
 
     test("first of two commands, Windows", () => {
@@ -147,10 +150,12 @@ suite("scrapbook parsing Tests", () => {
 
     test("first of two commands, Windows, on blank line before second command", () => {
         let line1 = "db.find()";
-        let line2 = "db.insertOne({'a': 'b'})";
-        let text = `${line1}\r\n\r\n\r\n${line2}`;
-        let command = getCommandFromTextAtLocation(text, new Position(2, 0));
-        assert.equal(command.text, line1);
+        for (let i in [0, 1, 2]) {
+            let line2 = `db.insertOne({${wrapInQuotes("a", +i)}:1})`;
+            let text = `${line1}\r\n\r\n\r\n${line2}`;
+            let command = getCommandFromTextAtLocation(text, new Position(2, 0));
+            assert.equal(command.text, line1);
+        }
     });
 
     test("drop", () => {
@@ -172,17 +177,19 @@ suite("scrapbook parsing Tests", () => {
     });
 
     test("multi-line insert from #214", () => {
-        testParse(
-            `db.heroes.insert({\n"id": 2,\r\n"name": "Batman",\r\n\r\n"saying": "I'm Batman"\r})`,
-            {
-                collection: "heroes", name: "insert", args: [
-                    {
-                        id: 2,
-                        name: "Batman",
-                        saying: "I'm Batman"
-                    }
-                ]
-            });
+        for (let i in [0, 1, 2]) {
+            testParse(
+                `db.heroes.insert({\n${wrapInQuotes("id", +i)}: 2,\r\n${wrapInQuotes("name", +i)}: "Batman",\r\n\r\n${wrapInQuotes("saying", +i)}: "I'm Batman"\r})`,
+                {
+                    collection: "heroes", name: "insert", args: [
+                        {
+                            id: 2,
+                            name: "Batman",
+                            saying: "I'm Batman"
+                        }
+                    ]
+                });
+        }
     });
 
     test("find/project from #214", () => {
@@ -310,23 +317,6 @@ suite("scrapbook parsing Tests", () => {
         expectSingleCommand(`db..(1, "a");`);
         expectSingleCommand(`..c1(1, "a");`);
     });
-
-    // https://github.com/Microsoft/vscode-cosmosdb/issues/466
-    // test("Unquoted property names", () => {
-    //     testParse(
-    //         `db.heroes.find({ id: 2 }, { saying: 1 })`,
-    //         {
-    //             collection: "heroes", name: "find", args: [
-    //                 {
-    //                     id: 2
-    //                 },
-    //                 {
-    //                     saying: 1
-    //                 }
-    //             ]
-    //         });
-    // });
-
 
     test("multi-line insert from #214", () => {
         testParse(
@@ -517,10 +507,12 @@ suite("scrapbook parsing Tests", () => {
                 ]
             });
     });
-    test("test function call with single quotes", () => {
-        let text = `db.test1.insertMany({'name': 'First'})`;
-        let command = getCommandFromTextAtLocation(text, new Position(0, 0));
-        assert.deepEqual(command.argumentObjects, [{ name: "First" }]);
+    test("test function call with and without quotes", () => {
+        for (let i in [0, 1, 2]) {
+            let text = `db.test1.insertMany({${wrapInQuotes("name", +i)}: 'First' })`;
+            let command = getCommandFromTextAtLocation(text, new Position(0, 0));
+            assert.deepEqual(command.argumentObjects, [{ name: "First" }]);
+        }
     });
     test("test function call with numbers", () => {
         let text = `db.test1.insertMany({'name': 1010101})`;
@@ -596,6 +588,70 @@ suite("scrapbook parsing Tests", () => {
         assert.deepEqual(command.argumentObjects, ["Hello \\'there\\'"]);
     });
 
+    test("test nested property names (has dots in the name)", () => {
+        let text = `db.test1.find({"name.FirstName": 1})`;
+        let command = getCommandFromTextAtLocation(text, new Position(0, 0));
+        assert.deepEqual(command.argumentObjects, [{ "name.FirstName": 1 }]);
+    });
+
+    test("test managed namespace collection names (has dots in the name)", () => {
+        let text = `db.test1.beep.find({})`;
+        let command = getCommandFromTextAtLocation(text, new Position(0, 0));
+        assert.deepEqual(command.collection, "test1.beep");
+    });
+
+    test("test user issues: https://github.com/Microsoft/vscode-cosmosdb/issues/688", () => {
+        for (let i in [0, 1, 2]) {
+            let text = `db.hdr.aggregate([
+                { ${wrapInQuotes("$match", +i)}: { "CURRENCY_ID": "USD" } },
+              ])`; //Note the trailing comma. There should be 1 argument object returned, an array, that has 2 elements
+            //one expected, and another empty object.
+            let command = getCommandFromTextAtLocation(text, new Position(0, 0));
+            assert.deepEqual(command.collection, "hdr");
+            assert.deepEqual(command.argumentObjects, [[{ $match: { "CURRENCY_ID": "USD" } }, {}]]);
+        }
+    });
+
+    test("test user issues: https://github.com/Microsoft/vscode-cosmosdb/issues/703", () => {
+        for (let i in [0, 1, 2]) {
+            let text = `db.Users.find({ ${wrapInQuotes("user", +i)}: { ${wrapInQuotes("$in", +i)}: [ "A80", "HPA" ] } },{ ${wrapInQuotes("_id", +i)}: false });`;
+            let command = getCommandFromTextAtLocation(text, new Position(0, 0));
+            assert.deepEqual(command.collection, "Users");
+            assert.deepEqual(command.argumentObjects, [{ user: { "$in": ["A80", "HPA"] } }, { _id: false }]);
+        }
+    });
+
+    test("test user issues: https://github.com/Microsoft/vscode-cosmosdb/issues/691", () => {
+        for (let i in [0, 1, 2]) {
+            let text = `db.users.aggregate([
+                { ${wrapInQuotes("$match", +i)}: {${wrapInQuotes("_id", +i)}: {"$oid" :"5b23d2ba92b52cf794bdeb9c")}}},
+                { ${wrapInQuotes("$project", +i)}: {
+                    ${wrapInQuotes("scores", +i)}: {${wrapInQuotes("$filter", +i)}: {
+                        ${wrapInQuotes("input", +i)}: '$scores',
+                        ${wrapInQuotes("as", +i)}: 'score',
+                        ${wrapInQuotes("cond", +i)}: {${wrapInQuotes("$gt", +i)}: ['$$score', 3]}
+                    }}
+                }}
+            ])`;
+            let command = getCommandFromTextAtLocation(text, new Position(0, 0));
+            assert.deepEqual(command.collection, "users");
+            assert.deepEqual(command.argumentObjects[0][0], { $match: { _id: { "$oid": "5b23d2ba92b52cf794bdeb9c" } } });
+            assert.deepEqual(command.argumentObjects[0][1],
+                {
+                    $project: {
+                        scores: {
+                            $filter: {
+                                input: '$scores',
+                                as: 'score',
+                                cond: { $gt: ['$$score', 3] }
+                            }
+                        }
+                    }
+                });
+        }
+    });
+
+
     //This test will fail. See https://github.com/Microsoft/vscode-cosmosdb/issues/689
     // test("test incomplete function call - replicate user typing - no function call yet", () => {
     //     let text = `db.test1.`;
@@ -604,15 +660,6 @@ suite("scrapbook parsing Tests", () => {
     //     assert.deepEqual(command.collection, "test1");
     // });
 
-    /* This test will fail. Unquoted strings need to be fixed.
-    test("test function call with no quotes", () => {
-        let arg0 = `{name: 'First'}`;
-        let text = `db.test1.insertMany(${arg0})`;
-        let command = getCommandFromTextAtLocation(text, new Position(0, 0));
-        const argument = command.argumentObjects[0];
-        assert.deepEqual(argument, { name: "First" });
-    });
-    */
 
 });
 

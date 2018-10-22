b94a32592e2c3a932109089863484f44611b284d
Nicolas Kyriazopuolos-Panagiotopoulos <nikolaskp@outlook.com>
2018-10-22 18:17:05 +0200

    Refresh tree after running mongo command (#856)

--- src/mongo/MongoScrapbook.ts
+++ src/mongo/MongoScrapbook.ts
@@ -111,6 +111,7 @@ async function executeCommand(activeEditor: vscode.TextEditor, database: MongoDa
 			const err = command.errors[0];
 			throw new Error(`Error near line ${err.range.start.line}, column ${err.range.start.character}: '${err.message}'. Please check syntax.`);
 		}
+
 		if (command.name === 'find') {
 			await editorManager.showDocument(new MongoFindResultEditor(database, command), 'cosmos-result.json', { showInNextColumn: true });
 		} else {
@@ -122,10 +123,23 @@ async function executeCommand(activeEditor: vscode.TextEditor, database: MongoDa
 				await editorManager.showDocument(new MongoFindOneResultEditor(database, command.collection, result), 'cosmos-result.json', { showInNextColumn: true });
 			} else {
 				await vscodeUtil.showNewFile(result, extensionPath, 'result', '.json', activeEditor.viewColumn + 1);
+				await refreshTreeAfterCommand(database, command);
 			}
 		}
 	} else {
 		throw new Error('No MongoDB command found at the current cursor location.');
+	}
+}
+
+async function refreshTreeAfterCommand(database: MongoDatabaseTreeItem, command: MongoCommand) {
+	if (command.name === 'drop') {
+		database.refresh();
+	}
+	else if (command.collection && /^(insert|update|delete|replace|remove|write|bulkWrite)/i.test(command.name)) {
+		const collectionNode = await ext.tree.findTreeItem(database.fullId + "/" + command.collection);
+		if (collectionNode) {
+			collectionNode.refresh();
+		}
 	}
 }
 

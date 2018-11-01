1c1ce7b65ee3121508db17c42b6a17bb125c54e3
Prashanth <30420973+PrashanthCorp@users.noreply.github.com>
2018-11-01 10:31:20 -0700

    Mongo connection string - account for emulator when adding or getting database name (#894)

--- src/constants.ts
+++ src/constants.ts
@@ -34,3 +34,5 @@ export const defaultStoredProcedure =
 };` ;
 
 export const emptyPartitionKeyValue = {};
+
+export let emulatorPassword = 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==';

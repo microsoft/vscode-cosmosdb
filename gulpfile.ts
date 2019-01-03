2d0bedbb5fa7dc444cfae186fb887bdb3c0e7f11
Eric Jizba <erijiz@microsoft.com>
2019-01-03 11:50:22 -0800

    Use typescript for gulp and fix builds (#987)
    Mac builds are failing since they're using Node 6. Fix this with the NodeTool task. Most of this "clean up" is just copied from functions repo.
    
    Also use typescript for gulp file

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,24 +3,24 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const gulp = require('gulp');
-const decompress = require('gulp-decompress');
-const download = require('gulp-download');
-const path = require('path');
-const os = require('os');
-const cp = require('child_process');
-const glob = require('glob');
-
+import * as cp from 'child_process';
+import * as glob from 'glob';
+import * as gulp from 'gulp';
+import * as decompress from 'gulp-decompress';
+import * as download from 'gulp-download';
+import * as os from 'os';
+import * as path from 'path';
 
 /**
  * Installs the azure account extension before running tests (otherwise our extension would fail to activate)
  * NOTE: The version isn't super important since we don't actually use the account extension in tests
  */
-gulp.task('install-azure-account', () => {
+function installAzureAccount() {
     const version = '0.4.3';
     const extensionPath = path.join(os.homedir(), `.vscode/extensions/ms-vscode.azure-account-${version}`);
     const existingExtensions = glob.sync(extensionPath.replace(version, '*'));
     if (existingExtensions.length === 0) {
+        // tslint:disable-next-line:no-http-string
         return download(`http://ms-vscode.gallery.vsassets.io/_apis/public/gallery/publisher/ms-vscode/extension/azure-account/${version}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage`)
             .pipe(decompress({
                 filter: file => file.path.startsWith('extension/'),
@@ -31,18 +31,18 @@ gulp.task('install-azure-account', () => {
             }))
             .pipe(gulp.dest(extensionPath));
     } else {
-        console.log("Azure Account extension already installed.")
+        console.log("Azure Account extension already installed.");
         // We need to signal to gulp that we've completed this async task
         return Promise.resolve();
     }
-});
+}
 
-
-gulp.task('test', gulp.series('install-azure-account', () => {
+function test() {
     const env = process.env;
-    env.DEBUGTELEMETRY = 1;
+    env.DEBUGTELEMETRY = '1';
     env.MOCHA_reporter = 'mocha-junit-reporter';
     env.MOCHA_FILE = path.join(__dirname, 'test-results.xml');
-    const cmd = cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
-    return cmd;
-}));
+    return cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
+}
+
+exports.test = gulp.series(installAzureAccount, test);

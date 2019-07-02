5b9ec47e88342944d0624c5db858a795ef961773
Stephen Weatherford <stephwe@microsoft.com>
2019-07-01 17:32:30 -0700

    work

--- test/mongo.test.ts
+++ test/mongo.test.ts
@@ -1,19 +1,19 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// /*---------------------------------------------------------------------------------------------
+//  *  Copyright (c) Microsoft Corporation. All rights reserved.
+//  *  Licensed under the MIT License. See License.txt in the project root for license information.
+//  *--------------------------------------------------------------------------------------------*/
 
-import * as assert from 'assert';
-import { ObjectID, ObjectId } from '../extension.bundle';
-import { Position } from 'vscode';
-import { parseError } from 'vscode-azureextensionui';
-import { MongoCommand } from '../extension.bundle';
-import * as cp from "child_process";
-import { getAllCommandsFromText, getCommandFromTextAtLocation } from '../extension.bundle';
+// import * as assert from 'assert';
+// import { ObjectID, ObjectId } from '../extension.bundle';
+// import { Position } from 'vscode';
+// import { parseError } from 'vscode-azureextensionui';
+// import { MongoCommand } from '../extension.bundle';
+// import * as cp from "child_process";
+// import { getAllCommandsFromText, getCommandFromTextAtLocation } from '../extension.bundle';
 
-suite("Mongo shell tests", () => {
-    let child = cp.exec("mongod");
-    child.on("data", (buffer: Buffer) => {
-        console.log(buffer.toString())
-    });
-});
+// suite("Mongo shell tests", () => {
+//     let child = cp.exec("mongod");
+//     child.on("data", (buffer: Buffer) => {
+//         console.log(buffer.toString())
+//     });
+// });

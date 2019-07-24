96910eebfd2b081780d940c27f64ea6282e7912f
Stephen Weatherford <stephwe@microsoft.com>
2019-07-23 17:01:40 -0700

    work

--- test/mongo.test.ts
+++ test/mongo.test.ts
@@ -1,29 +1,42 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// /*---------------------------------------------------------------------------------------------
+//  *  Copyright (c) Microsoft Corporation. All rights reserved.
+//  *  Licensed under the MIT License. See License.txt in the project root for license information.
+//  *--------------------------------------------------------------------------------------------*/
 
-import * as assert from 'assert';
-import { ObjectID, ObjectId } from '../extension.bundle';
-import { Position } from 'vscode';
-import { parseError } from 'vscode-azureextensionui';
-import { MongoCommand } from '../extension.bundle';
-import * as cp from "child_process";
-import { getAllCommandsFromText, getCommandFromTextAtLocation } from '../extension.bundle';
+// import * as assert from 'assert';
+// import { ObjectID, ObjectId } from '../extension.bundle';
+// import { Position } from 'vscode';
+// import { parseError } from 'vscode-azureextensionui';
+// import { MongoCommand } from '../extension.bundle';
+// import * as cp from "child_process";
+// import { getAllCommandsFromText, getCommandFromTextAtLocation } from '../extension.bundle';
 
-suite("Mongo shell tests", () => {
-    suiteSetup(() => {
-        let child = cp.exec("mongod");
+// suite("MongoShell", () => {
+//     let mongodCP: cp.ChildProcess;
 
-        child.on("data", (buffer: Buffer) => {
-            console.log("mongod: " + buffer.toString());
-        });
-        child.on("error", (buffer: Buffer) => {
-            console.log("mongod Error: " + buffer.toString());
-        });
-        child.on("close", (code?: number) => {
-            console.log("mongod: Close " + code);
-        });
-    });
+//     suiteSetup(() => {
+//         mongodCP = cp.spawn(
+//             "mongod2",
+//             [],
+//             {
+//                 shell: true
+//             });
 
-});
+//         mongodCP.on("data", (buffer: Buffer) => {
+//             console.log("mongod: " + buffer.toString());
+//         });
+//         mongodCP.on("error", (buffer: Buffer) => {
+//             console.log("mongod Error: " + buffer.toString());
+//         });
+//         mongodCP.on("close", (code?: number) => {
+//             console.log("mongod: Close " + code);
+//         });
+//     });
+
+//     test("a", () => {
+
+//     });
+//     suiteTeardown(() => {
+//         mongodCP.kill();
+//     });
+// });

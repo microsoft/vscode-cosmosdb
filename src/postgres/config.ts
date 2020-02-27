27dd5b2b469622ef38ef3a2080a0ca40c5c1ae32
Neelima Potharaj <neelimap@MININT-88OMGE1.redmond.corp.microsoft.com>
2020-02-26 23:03:50 -0800

    tests are successful

---
+++ src/postgres/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+    user: 'nepothar@test-azure-postgresql',
+    password: 'ZXcfvg!@34',
+    ssl: false
+};
---
+++ src/postgres/connectToPostgresClient.ts
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Client } from 'pg';
+
+export async function connectToPostgresClient(clientConfig): Promise<Client> {
+
+    const client = new Client(clientConfig);
+    try {
+        return client;
+    } catch (err) {
+        const error = <{ message?: string, name?: string }>err;
+        throw error;
+    }
+}

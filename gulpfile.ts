af3e1f9f0ffe95028d403068b54cee596658a66a
Eric Jizba <erijiz@microsoft.com>
2020-03-17 15:56:16 -0700

    Easier debugging in a webpacked world 🕸📦🌎 (#1369)

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,10 +3,22 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+// tslint:disable:no-implicit-dependencies (this allows the use of dev dependencies)
+
 import * as cp from 'child_process';
+import * as fse from 'fs-extra';
 import * as gulp from 'gulp';
 import * as path from 'path';
 import { gulp_installAzureAccount, gulp_webpack } from 'vscode-azureextensiondev';
+
+async function prepareForWebpack(): Promise<void> {
+    const mainJsPath: string = path.join(__dirname, 'main.js');
+    let contents: string = (await fse.readFile(mainJsPath)).toString();
+    contents = contents
+        .replace('out/src/extension', 'dist/extension.bundle')
+        .replace(', true /* ignoreBundle */', '');
+    await fse.writeFile(mainJsPath, contents);
+}
 
 function test(): cp.ChildProcess {
     const env = process.env;
@@ -16,6 +28,6 @@ function test(): cp.ChildProcess {
     return cp.spawn('node', ['./node_modules/vscode/bin/test'], { stdio: 'inherit', env });
 }
 
-exports['webpack-dev'] = () => gulp_webpack('development');
-exports['webpack-prod'] = () => gulp_webpack('production');
+exports['webpack-dev'] = gulp.series(prepareForWebpack, () => gulp_webpack('development'));
+exports['webpack-prod'] = gulp.series(prepareForWebpack, () => gulp_webpack('production'));
 exports.test = gulp.series(gulp_installAzureAccount, test);

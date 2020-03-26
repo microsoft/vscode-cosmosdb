2c44eec81c758503a711abac0fc8a608ca2a3f68
v-wuzhai <46013274+v-wuzhai@users.noreply.github.com>
2020-03-25 19:21:45 -0700

    Create graph account (#1349)
    Co-authored-by: unknown <AzureTest@vwuzhai002.redmond.corp.microsoft.com>

---
+++ test/nightly/azureResourceGraph.test.ts
@@ -0,0 +1,38 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as assert from 'assert';
+import { CosmosDBManagementModels } from 'azure-arm-cosmosdb';
+import { IHookCallbackContext, ISuiteCallbackContext } from 'mocha';
+import * as vscode from 'vscode';
+import { randomUtils } from '../../extension.bundle';
+import { longRunningTestsEnabled, testUserInput } from '../global.test';
+import { client, resourceGroupsToDelete } from './global.resource.test';
+
+suite('Graph action', async function (this: ISuiteCallbackContext): Promise<void> {
+    this.timeout(20 * 60 * 1000);
+    let resourceGroupName: string;
+    let accountName: string;
+
+    suiteSetup(async function (this: IHookCallbackContext): Promise<void> {
+        if (!longRunningTestsEnabled) {
+            this.skip();
+        }
+        this.timeout(2 * 60 * 1000);
+        resourceGroupName = randomUtils.getRandomHexString(12);
+        // Cosmos DB account must have lower case name
+        accountName = randomUtils.getRandomHexString(12).toLowerCase();
+        resourceGroupsToDelete.push(resourceGroupName);
+    });
+
+    test('Create graph account', async () => {
+        const testInputs: (string | RegExp)[] = [accountName, /graph/, '$(plus) Create new resource group', resourceGroupName, 'West US'];
+        await testUserInput.runWithInputs(testInputs, async () => {
+            await vscode.commands.executeCommand('cosmosDB.createAccount');
+        });
+        const getAccount: CosmosDBManagementModels.DatabaseAccount | undefined = await client.databaseAccounts.get(resourceGroupName, accountName);
+        assert.ok(getAccount);
+    });
+});

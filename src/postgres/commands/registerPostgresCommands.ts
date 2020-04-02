a30b1896d6504a286362032ab162e6cc8ac2eb23
William Lorey <wilorey@microsoft.com>
2020-04-02 11:11:08 -0700

    Move firewall command to commands folder

--- src/postgres/commands/registerPostgresCommands.ts
+++ src/postgres/commands/registerPostgresCommands.ts
@@ -4,8 +4,10 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { registerCommand } from "vscode-azureextensionui";
+import { configurePostgresFirewall } from "./configurePostgresFirewall";
 import { enterPostgresCredentials } from "./enterPostgresCredentials";
 
 export function registerPostgresCommands(): void {
     registerCommand('cosmosDB.enterPostgresCredentials', enterPostgresCredentials);
+    registerCommand('cosmosDB.configurePostgresFirewall', configurePostgresFirewall);
 }

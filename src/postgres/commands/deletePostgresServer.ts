8173994c3eafeb0948d3a5977e8347729497058d
nepothar <nepothar@microsoft.com>
2020-04-06 17:17:53 -0700

    fix to deletePostgresServer function and null-ref bug

--- src/postgres/commands/deletePostgresServer.ts
+++ src/postgres/commands/deletePostgresServer.ts
@@ -8,13 +8,13 @@ import { DialogResponses, IActionContext } from 'vscode-azureextensionui';
 import { ext } from '../../extensionVariables';
 import { PostgresServerTreeItem } from '../tree/PostgresServerTreeItem';
 
-export async function deletePostgresServer(context: IActionContext, node: PostgresServerTreeItem): Promise<void> {
-    const message: string = `Are you sure you want to delete server '${node.label}' and its contents?`;
+export async function deletePostgresServer(context: IActionContext, node?: PostgresServerTreeItem): Promise<void> {
+    if (!node) {
+        node = <PostgresServerTreeItem>await ext.tree.showTreeItemPicker(PostgresServerTreeItem.contextValue, context);
+    }
+    const message: string = `Are you sure you want to delete server "${node.name}" and its contents?`;
     const result = await ext.ui.showWarningMessage(message, { modal: true }, DialogResponses.deleteResponse);
     if (result === DialogResponses.deleteResponse) {
-        if (!node) {
-            node = <PostgresServerTreeItem>await ext.tree.showTreeItemPicker(PostgresServerTreeItem.contextValue, context);
-        }
         await node.deleteTreeItem(context);
         vscode.window.showInformationMessage(`Successfully deleted server "${node.name}".`);
     }

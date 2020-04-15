fcce1610eba6b296ecb552ab20515d2ce7367ade
William Lorey <wilorey@microsoft.com>
2020-04-15 15:44:21 -0700

    Add support for deleting Postgres functions

---
+++ src/postgres/commands/deletePostgresFunction.ts
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { window } from 'vscode';
+import { DialogResponses, IActionContext } from "vscode-azureextensionui";
+import { ext } from "../../extensionVariables";
+import { localize } from "../../utils/localize";
+import { PostgresFunctionTreeItem } from "../tree/PostgresFunctionTreeItem";
+
+export async function deletePostgresFunction(context: IActionContext, treeItem?: PostgresFunctionTreeItem): Promise<void> {
+    if (!treeItem) {
+        treeItem = <PostgresFunctionTreeItem>await ext.tree.showTreeItemPicker(PostgresFunctionTreeItem.contextValue, context);
+    }
+
+    const message = localize('deleteFunction', 'Are you sure you want to delete function "{0}"?', treeItem.label);
+    await ext.ui.showWarningMessage(message, { modal: true }, DialogResponses.deleteResponse);
+    await treeItem.deleteTreeItem(context);
+    window.showInformationMessage(localize('successfullyDeletedFunction', 'Successfully deleted function "{0}".', treeItem.label));
+}

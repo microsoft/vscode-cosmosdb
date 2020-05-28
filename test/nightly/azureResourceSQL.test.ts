73d2baaf80b3bd9ddb56531d78c1c1ed4cd2fa22
William Lorey <wilorey@microsoft.com>
2020-05-28 12:11:46 -0700

    Update tests for 'azureDatabases.createServer' (not including Postgres)

--- test/nightly/azureResourceSQL.test.ts
+++ test/nightly/azureResourceSQL.test.ts
@@ -33,9 +33,9 @@ suite('SQL action', async function (this: Mocha.Suite): Promise<void> {
     });
 
     test('Create SQL account', async () => {
-        const testInputs: (string | RegExp)[] = [accountName, /SQL/, '$(plus) Create new resource group', resourceGroupName, 'West US'];
+        const testInputs: (string | RegExp)[] = [/SQL/, accountName, '$(plus) Create new resource group', resourceGroupName, 'West US'];
         await testUserInput.runWithInputs(testInputs, async () => {
-            await vscode.commands.executeCommand('cosmosDB.createAccount');
+            await vscode.commands.executeCommand('azureDatabases.createServer');
         });
         const getAccount: CosmosDBManagementModels.DatabaseAccount | undefined = await client.databaseAccounts.get(resourceGroupName, accountName);
         assert.ok(getAccount);

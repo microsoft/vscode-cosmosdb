20b2c4ebb607ebae93149df9eda1d33080ed37ef
Will Lorey <wilorey@microsoft.com>
2020-06-08 22:16:51 -0700

    Add tree view for Postgres 🐘 stored procedures ⚙️ (#1562)

--- src/postgres/tree/PostgresStoredProcedureTreeItem.ts
+++ src/postgres/tree/PostgresStoredProcedureTreeItem.ts
@@ -3,17 +3,15 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Client } from "pg";
 import { AzureTreeItem, ISubscriptionContext, TreeItemIconPath } from "vscode-azureextensionui";
 import { getThemedIconPath } from "../../constants";
 import { IPostgresProceduresQueryRow } from "../getPostgresProcedureQueryRows";
-import { PostgresFunctionsTreeItem } from "./PostgresFunctionsTreeItem";
+import { PostgresStoredProceduresTreeItem } from "./PostgresStoredProceduresTreeItem";
 
-export class PostgresFunctionTreeItem extends AzureTreeItem<ISubscriptionContext> {
-    public static contextValue: string = 'postgresFunction';
-    public readonly contextValue: string = PostgresFunctionTreeItem.contextValue;
-    public readonly commandId: string = 'postgreSQL.openFunction';
-    public readonly parent: PostgresFunctionsTreeItem;
+export class PostgresStoredProcedureTreeItem extends AzureTreeItem<ISubscriptionContext> {
+    public static contextValue: string = 'postgresStoredProcedure';
+    public readonly contextValue: string = PostgresStoredProcedureTreeItem.contextValue;
+    public readonly parent: PostgresStoredProceduresTreeItem;
     public readonly schema: string;
     public readonly name: string;
     public readonly id: string;
@@ -21,7 +19,7 @@ export class PostgresFunctionTreeItem extends AzureTreeItem<ISubscriptionContext
     public readonly isDuplicate: boolean;
     public definition: string;
 
-    constructor(parent: PostgresFunctionsTreeItem, row: IPostgresProceduresQueryRow, isDuplicate: boolean) {
+    constructor(parent: PostgresStoredProceduresTreeItem, row: IPostgresProceduresQueryRow, isDuplicate: boolean) {
         super(parent);
         this.schema = row.schema;
         this.name = row.name;
@@ -40,16 +38,6 @@ export class PostgresFunctionTreeItem extends AzureTreeItem<ISubscriptionContext
     }
 
     public get iconPath(): TreeItemIconPath {
-        return getThemedIconPath('function.svg');
-    }
-
-    public async deleteTreeItemImpl(): Promise<void> {
-        const client = new Client(this.parent.clientConfig);
-        try {
-            await client.connect();
-            await client.query(`DROP FUNCTION ${this.schema}.${this.name}(${this.args});`);
-        } finally {
-            await client.end();
-        }
+        return getThemedIconPath('Process_16x.svg');
     }
 }

3581c85b892c8becd712dfd365f9d2b7faab3c2a
Will Lorey <wilorey@microsoft.com>
2020-06-24 00:28:20 +0000

    Return a new collection node if one doesn't exist (#1582)

--- src/DatabasesFileSystem.ts
+++ src/DatabasesFileSystem.ts
@@ -3,11 +3,14 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { Collection, Db } from "mongodb";
+import { basename, dirname } from 'path';
 import { FileStat, FileType, MessageItem, Uri, workspace } from "vscode";
 import { AzExtItemQuery, AzExtItemUriParts, AzExtTreeFileSystem, AzExtTreeItem, DialogResponses, IActionContext, UserCancelledError } from 'vscode-azureextensionui';
 import { FileChangeType } from "vscode-languageclient";
 import { ext } from "./extensionVariables";
 import { MongoCollectionTreeItem } from "./mongo/tree/MongoCollectionTreeItem";
+import { MongoDatabaseTreeItem } from "./mongo/tree/MongoDatabaseTreeItem";
 import { localize } from "./utils/localize";
 import { getWorkspaceSetting, updateGlobalSetting } from "./utils/settingUtils";
 import { getNodeEditorLabel } from "./utils/vscodeUtils";
@@ -84,7 +87,18 @@ export class DatabasesFileSystem extends AzExtTreeFileSystem<IEditableTreeItem> 
     }
 
     protected async findItem(context: IActionContext, query: AzExtItemQuery): Promise<IEditableTreeItem | undefined> {
-        const node: IEditableTreeItem | undefined = await super.findItem(context, query);
+        let node: IEditableTreeItem | undefined = await super.findItem(context, query);
+        if (!node) {
+            const parentId: string = dirname(query.id);
+            const parentNode: IEditableTreeItem | undefined = await ext.tree.findTreeItem(parentId, context);
+            if (parentNode instanceof MongoDatabaseTreeItem) {
+                const db: Db = await parentNode.connectToDb();
+                const collectionName: string = basename(query.id);
+                const collection: Collection = db.collection(collectionName);
+                node = new MongoCollectionTreeItem(parentNode, collection);
+            }
+        }
+
         if (node && node instanceof MongoCollectionTreeItem) {
             const findArgs: {}[] | undefined = getFindArgsFromQuery(query);
             if (findArgs) {

e27193f8188a95bc674242c29ec4ca165113caf2
nepothar <nepothar@microsoft.com>
2020-08-10 18:04:10 -0700

    merge fixes

--- src/postgres/commands/copyConnectionString.ts
+++ src/postgres/commands/copyConnectionString.ts
@@ -8,7 +8,7 @@ import * as vscode from 'vscode';
 import { IActionContext } from 'vscode-azureextensionui';
 import { ext } from '../../extensionVariables';
 import { localize } from '../../utils/localize';
-import { createPostgresConnectionString } from '../postgresConnectionStrings';
+import { nonNullProp } from '../../utils/nonNull';
 import { PostgresDatabaseTreeItem } from '../tree/PostgresDatabaseTreeItem';
 import { checkAuthentication } from './checkAuthentication';
 
@@ -22,4 +22,14 @@ export async function copyConnectionString(context: IActionContext, node: Postgr
     await vscode.env.clipboard.writeText(connectionString);
     const message = localize('copiedPostgresConnectStringMsg', 'The connection string has been copied to the clipboard');
     vscode.window.showInformationMessage(message);
+}
+
+async function createPostgresConnectionString(clientConfig: ClientConfig): Promise<string> {
+
+    const user: string = nonNullProp(clientConfig, 'user');
+    const password: string = nonNullProp(clientConfig, 'password');
+    const host: string = nonNullProp(clientConfig, 'host');
+    const port: string = String(nonNullProp(clientConfig, 'port'));
+    return `postgres://${user}:${password}@${host}:${port}/'${clientConfig.database}'`;
+
 }

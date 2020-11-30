815e33718376237eadfa672f6316d3276680c68c
nepothar <nepothar@microsoft.com>
2020-11-30 12:00:10 -0800

    added new "agent" property to CosmosClient

--- src/docdb/getCosmosClient.ts
+++ src/docdb/getCosmosClient.ts
@@ -4,8 +4,15 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { CosmosClient } from "@azure/cosmos";
+import * as https from "https";
+import * as vscode from 'vscode';
 import { appendExtensionUserAgent } from "vscode-azureextensionui";
+import { ext } from "../extensionVariables";
 
-export function getCosmosClient(endpoint: string, key: string): CosmosClient {
-    return new CosmosClient({ endpoint, key, userAgentSuffix: appendExtensionUserAgent() });
+export function getCosmosClient(endpoint: string, key: string, isEmulator: boolean | undefined): CosmosClient {
+
+    const vscodeStrictSSL: boolean | undefined = vscode.workspace.getConfiguration().get<boolean>(ext.settingsKeys.vsCode.proxyStrictSSL);
+    const strictSSL = !isEmulator && vscodeStrictSSL;
+    return new CosmosClient({ endpoint, key, userAgentSuffix: appendExtensionUserAgent(), agent: new https.Agent({ rejectUnauthorized: !strictSSL }) });
+
 }

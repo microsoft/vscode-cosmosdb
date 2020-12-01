8f815429f9f54e0bc11c375d8eb6df5ad0341084
nepothar <nepothar@microsoft.com>
2020-12-01 12:21:01 -0800

    added "toLowerCase" to "PG_" condition

--- src/postgres/commands/createPostgresServer/steps/PostgresServerCredUserStep.ts
+++ src/postgres/commands/createPostgresServer/steps/PostgresServerCredUserStep.ts
@@ -39,7 +39,7 @@ async function validateUser(username: string): Promise<string | undefined> {
         return localize('usernameCharacterCheck', 'The name can only contain letters, numbers, and the "_" character.');
     } else if (username.match(/^[0-9]+/)) {
         return localize('usernameBeginningMatch', 'The name cannot start with a number.');
-    } else if (username.startsWith('pg_')) {
+    } else if (username.toLowerCase().startsWith('pg_')) {
         return localize('usernameStartWithCheck', 'Admin username cannot start with "pg_".');
     } else if (restricted.includes(username.toLowerCase())) {
         const restrictedString = restricted.map(d => `"${d}"`).join(', ');

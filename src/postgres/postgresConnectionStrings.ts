2e92cb28b876a6bff2261eed0f4b76df9720ba6f
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2021-12-28 03:29:30 +0000

    Add camelCase convection

--- src/postgres/postgresConnectionStrings.ts
+++ src/postgres/postgresConnectionStrings.ts
@@ -25,8 +25,8 @@ export function createPostgresConnectionString(hostName: string, port: string = 
         const encodedUsername = encodeURIComponent(username);
         if (password) {
             const encodedPassword = encodeURIComponent(password);
-            const encodedPasswordwithQuotes = "'" + encodedPassword + "'";
-            connectionString += `${encodedUsername}:${encodedPasswordwithQuotes}@`;
+            const encodedPasswordWithQuotes = "'" + encodedPassword + "'";
+            connectionString += `${encodedUsername}:${encodedPasswordWithQuotes}@`;
         } else {
             connectionString += `${encodedUsername}@`;
         }

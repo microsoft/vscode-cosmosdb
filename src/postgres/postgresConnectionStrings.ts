098e42d4f132416897e3c2d371ea8cf35e1a25fa
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2021-12-28 02:45:10 +0000

    Postgress Password

--- src/postgres/postgresConnectionStrings.ts
+++ src/postgres/postgresConnectionStrings.ts
@@ -25,7 +25,8 @@ export function createPostgresConnectionString(hostName: string, port: string = 
         const encodedUsername = encodeURIComponent(username);
         if (password) {
             const encodedPassword = encodeURIComponent(password);
-            connectionString += `${encodedUsername}:${encodedPassword}@`;
+            const encodedPasswordwithQuotes = "'"+encodedPassword+"'";
+            connectionString += `${encodedUsername}:${encodedPasswordwithQuotes}@`;
         } else {
             connectionString += `${encodedUsername}@`;
         }

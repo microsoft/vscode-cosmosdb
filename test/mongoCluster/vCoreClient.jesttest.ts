175d6cc645244ac82c4568b771a7bef50a789914
Tomasz Naumowicz <tnaum-ms@naumowicz.de>
2024-08-26 15:22:31 +0200

    Added 'jest' testing framework, with separate configs for tests in order to maintain existing mocha infra for legacy tests

---
+++ test/mongoCluster/vCoreClient.jesttest.ts
@@ -0,0 +1,9 @@
+/// <reference types="jest" />
+
+import { VCoreClient } from '../../src/vCore/VCoreClient';
+
+describe('vCoreClient', () => {
+    it('should be defined', () => {
+        expect(VCoreClient).toBeDefined();
+    });
+});

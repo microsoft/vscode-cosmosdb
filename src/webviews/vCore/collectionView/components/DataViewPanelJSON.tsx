3bc138c5db4f7318bd7788a3c003dc7186efde26
Tomasz Naumowicz <tnaum-ms@naumowicz.de>
2024-09-09 08:16:32 +0200

    vCore: added monaco with JSON support to the collection view webview (#2309)

--- src/webviews/vCore/collectionView/components/DataViewPanelJSON.tsx
+++ src/webviews/vCore/collectionView/components/DataViewPanelJSON.tsx
@@ -1,11 +1,37 @@
-import { Textarea } from '@fluentui/react-components';
+import Editor, { loader } from '@monaco-editor/react';
 import * as React from 'react';
+// eslint-disable-next-line import/no-internal-modules
+import * as monacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
 
 interface Props {
     value: string;
 }
 
-export const DataViewPanelJSON = ({ value } : Props ): React.JSX.Element => {
+loader.config({ monaco: monacoEditor });
+
+const monacoOptions = {
+    // autoIndent: 'full',
+    // contextmenu: true,
+    // fontFamily: 'monospace',
+    // fontSize: 13,
+    // lineHeight: 24,
+    // hideCursorInOverviewRuler: true,
+    // matchBrackets: 'always',
+    minimap: {
+        enabled: true,
+    },
+    // scrollbar: {
+    //   horizontalSliderSize: 4,
+    //   verticalSliderSize: 18,
+    // },
+    // selectOnLineNumbers: true,
+    // roundedSelection: false,
+    readOnly: true,
+    // cursorStyle: 'line',
+    // automaticLayout: true,
+};
+
+export const DataViewPanelJSON = ({ value }: Props): React.JSX.Element => {
     React.useEffect(() => {
         console.log('JSON View has mounted');
 
@@ -15,9 +41,11 @@ export const DataViewPanelJSON = ({ value } : Props ): React.JSX.Element => {
     }, []); // Empty dependency array means this runs only once, like componentDidMount
 
     return (
-        <Textarea
-            style={{ height: '100%', width: '100%' }}
-            textarea={{ style: { maxHeight: 'unset' } }}
+        <Editor
+            height={'100%'}
+            width={'100%'}
+            language="json"
+            options={monacoOptions}
             value={value}
         />
     );

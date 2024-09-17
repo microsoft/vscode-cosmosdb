7638a482a2992d5a3a666873e453cf3917eea08a
Dmitry Shilov <dshilov@microsoft.com>
2024-09-17 11:00:09 +0200

    chore: Adaptive theme supports VSCode dark themes (#2320)

--- src/webviews/QueryEditor/QueryPanel/QueryMonaco.tsx
+++ src/webviews/QueryEditor/QueryPanel/QueryMonaco.tsx
@@ -6,7 +6,7 @@ import * as monacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
 import { useMemo, useState } from 'react';
 import { useThemeMutationObserver } from '../../theme/DynamicThemeProvider';
 import { useVSCodeTheme } from '../../theme/themeGenerator';
-import { useQueryEditorDispatcher, useQueryEditorState } from '../state/QueryEditorContext';
+import { useQueryEditorDispatcher, useQueryEditorState } from '../QueryEditorContext';
 
 loader.config({ monaco: monacoEditor });
 

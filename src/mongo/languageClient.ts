911f1ae06625ae7b55a96c04d278c29d506b5a0e
Dmitry Shilov <dshilov@microsoft.com>
2024-10-08 10:32:40 +0200

    chore: Introduces eslint license header plugin (#2332)
    - eslint checks copyright header in each ts(x) file
    - eslint --fix was ran. Fixed all files without copyright header

--- src/mongo/languageClient.ts
+++ src/mongo/languageClient.ts
@@ -2,6 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
 import { appendExtensionUserAgent } from '@microsoft/vscode-azext-utils';
 import * as path from 'path';
 import { LanguageClient, TransportKind, type LanguageClientOptions, type ServerOptions } from 'vscode-languageclient';

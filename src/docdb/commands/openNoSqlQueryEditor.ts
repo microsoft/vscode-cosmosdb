184a89bae69f2779a0d642bb2e33b9dfd2c1df45
Laurent Nguyen <laurent.nguyen@microsoft.com>
2024-10-08 14:42:18 +0200

    Minor syntax fix

--- src/docdb/commands/openNoSqlQueryEditor.ts
+++ src/docdb/commands/openNoSqlQueryEditor.ts
@@ -5,13 +5,14 @@
 
 import { type IActionContext } from '@microsoft/vscode-azext-utils';
 import { QueryEditorTab } from '../../panels/QueryEditorTab';
+import { TabBase } from '../../panels/TabBase';
 import { type DocDBCollectionTreeItem } from '../tree/DocDBCollectionTreeItem';
 import { createNoSqlQueryConnection } from './connectNoSqlContainer';
 
 export const openNoSqlQueryEditor = (_context: IActionContext, node?: DocDBCollectionTreeItem) => {
     const connection = node ? createNoSqlQueryConnection(node) : undefined;
 
-    QueryEditorTab.render<QueryEditorTab>(
+    TabBase.render<QueryEditorTab>(
         {
             c: QueryEditorTab,
             title: 'Query Editor',

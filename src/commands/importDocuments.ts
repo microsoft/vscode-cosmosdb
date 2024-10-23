cfb2ab42de4a4ce7d8623e351748eb65ebbc72ef
Tomasz Naumowicz <tnaum-ms@naumowicz.de>
2024-10-23 07:53:15 +0200

    vCore: import JSON as EJSON (adding BSON support)

--- src/commands/importDocuments.ts
+++ src/commands/importDocuments.ts
@@ -5,6 +5,7 @@
 
 import { type ItemDefinition } from '@azure/cosmos';
 import { parseError, type IActionContext } from '@microsoft/vscode-azext-utils';
+import { EJSON } from 'bson';
 import * as fse from 'fs-extra';
 import * as vscode from 'vscode';
 import { cosmosMongoFilter, sqlFilter } from '../constants';
@@ -60,7 +61,10 @@ export async function importDocuments(
             collectionNode = nonNullValue(collectionNode, 'collectionNode');
 
             progress.report({ increment: 20, message: 'Loading documents...' });
-            const documents: unknown[] = await parseDocuments(uris);
+
+            const supportEJSON: boolean = collectionNode instanceof CollectionItem; // added this line for better readability
+            const documents: unknown[] = await parseDocuments(uris, supportEJSON);
+
             progress.report({ increment: 30, message: `Loaded ${documents.length} document(s). Importing...` });
             if (collectionNode instanceof MongoCollectionTreeItem) {
                 result = await insertDocumentsIntoMongo(collectionNode, documents);
@@ -96,16 +100,31 @@ async function askForDocuments(context: IActionContext): Promise<vscode.Uri[]> {
     return await context.ui.showOpenDialog(openDialogOptions);
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-async function parseDocuments(uris: vscode.Uri[]): Promise<unknown[]> {
+/**
+ * Parses an array of URIs to read JSON documents and returns them as an array of unknown objects.
+ * If any errors are encountered while reading the documents, they are logged to the output channel.
+ *
+ * @param uris - An array of `vscode.Uri` objects representing the file paths to the JSON documents.
+ * @param supportEJSON - An optional boolean parameter that indicates whether to support extended JSON (EJSON).
+ *                       EJSON is used to read documents that are supposed to be converted into BSON.
+ *                       EJSON supports more datatypes and is specific to MongoDB. This is currently used for MongoDB clusters/vcore.
+ * @returns A promise that resolves to an array of parsed documents as unknown objects.
+ * @throws An error if any documents contain errors, prompting the user to fix them and try again.
+*/
+async function parseDocuments(uris: vscode.Uri[], supportEJSON: boolean = false): Promise<unknown[]> {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     let documents: any[] = [];
     let errorFoundFlag: boolean = false;
     for (const uri of uris) {
         let parsed;
         try {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-            parsed = await fse.readJSON(uri.fsPath);
+            if (supportEJSON) {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                parsed = EJSON.parse(await fse.readFile(uri.fsPath, 'utf8'));
+            } else {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                parsed = await fse.readJSON(uri.fsPath);
+            }
         } catch (e) {
             if (!errorFoundFlag) {
                 errorFoundFlag = true;

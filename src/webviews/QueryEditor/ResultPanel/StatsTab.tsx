9d806ef070cc49869549297b632e75b09211acb5
Dmitrii Shilov <dshilov@microsoft.com>
2024-11-08 12:51:46 +0100

    feat: Index Metrics moved to Stats tab

--- src/webviews/QueryEditor/ResultPanel/StatsTab.tsx
+++ src/webviews/QueryEditor/ResultPanel/StatsTab.tsx
@@ -23,12 +23,28 @@ const useStyles = makeStyles({
         paddingBottom: '10px',
     },
     bottomLabel: {
-        paddingTop: '20px',
         paddingBottom: '10px',
     },
     pre: {
-        margin: '0',
+        marginTop: '0',
         fontFamily: 'var(--fontFamilyMonospace)',
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        gap: '20px',
+    },
+    panel1: {
+        flexGrow: '1',
+        flexShrink: '1',
+        flexBasis: '68%',
+    },
+    panel2: {
+        flexGrow: '1',
+        flexShrink: '1',
+        flexBasis: '28%',
     },
 });
 
@@ -40,41 +56,47 @@ export const StatsTab = () => {
 
     return (
         <>
-            <div className={styles.topLabel}>
-                <Label size={'large'}>Query metrics</Label>
-            </div>
-            <Table arial-label="Stats table" style={{ minWidth: '510px' }}>
-                <TableHeader>
-                    <TableRow>
-                        <TableHeaderCell key={'metric'}>Metric</TableHeaderCell>
-                        <TableHeaderCell key={'value'}>Value</TableHeaderCell>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {items.map((item) => (
-                        <TableRow key={item.metric}>
-                            <TableCell>
-                                <TableCellLayout>{item.metric}</TableCellLayout>
-                            </TableCell>
-                            <TableCell>
-                                <TableCellLayout>
-                                    {!!item.tooltip && (
-                                        <Tooltip content={item.tooltip} relationship="description" withArrow>
-                                            <Label>{item.formattedValue}</Label>
-                                        </Tooltip>
-                                    )}{' '}
-                                    {!item.tooltip && <Label>{item.formattedValue}</Label>}
-                                </TableCellLayout>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-            <div className={styles.bottomLabel}>
-                <Label size={'large'}>Index metrics</Label>
-            </div>
-            <div>
-                <p className={styles.pre}>{indexMetrics}</p>
+            <div className={styles.container}>
+                <div className={styles.panel1}>
+                    <div className={styles.topLabel}>
+                        <Label size={'large'}>Query metrics</Label>
+                    </div>
+                    <Table arial-label="Stats table" style={{ minWidth: '510px' }}>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHeaderCell key={'metric'}>Metric</TableHeaderCell>
+                                <TableHeaderCell key={'value'}>Value</TableHeaderCell>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {items.map((item) => (
+                                <TableRow key={item.metric}>
+                                    <TableCell>
+                                        <TableCellLayout>{item.metric}</TableCellLayout>
+                                    </TableCell>
+                                    <TableCell>
+                                        <TableCellLayout>
+                                            {!!item.tooltip && (
+                                                <Tooltip content={item.tooltip} relationship="description" withArrow>
+                                                    <Label>{item.formattedValue}</Label>
+                                                </Tooltip>
+                                            )}{' '}
+                                            {!item.tooltip && <Label>{item.formattedValue}</Label>}
+                                        </TableCellLayout>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
+                <div className={styles.panel2}>
+                    <div className={styles.bottomLabel}>
+                        <Label size={'large'}>Index metrics</Label>
+                    </div>
+                    <div>
+                        <pre className={styles.pre}>{indexMetrics}</pre>
+                    </div>
+                </div>
             </div>
         </>
     );

a8dcb735bed4c136dd3ca7d8e3a91c1ab0d34bc9
Dmitrii Shilov <dshilov@microsoft.com>
2024-11-18 12:54:46 +0100

    feat: Add "Open Query Editor" item to the Documents node submenu

--- src/docdb/commands/openNoSqlQueryEditor.ts
+++ src/docdb/commands/openNoSqlQueryEditor.ts
@@ -6,10 +6,18 @@
 import { type IActionContext } from '@microsoft/vscode-azext-utils';
 import { QueryEditorTab } from '../../panels/QueryEditorTab';
 import { type DocDBCollectionTreeItem } from '../tree/DocDBCollectionTreeItem';
+import { DocDBDocumentsTreeItem } from '../tree/DocDBDocumentsTreeItem';
 import { createNoSqlQueryConnection } from './connectNoSqlContainer';
 
-export const openNoSqlQueryEditor = (_context: IActionContext, node?: DocDBCollectionTreeItem) => {
-    const connection = node ? createNoSqlQueryConnection(node) : undefined;
+export const openNoSqlQueryEditor = (
+    _context: IActionContext,
+    node?: DocDBCollectionTreeItem | DocDBDocumentsTreeItem,
+) => {
+    const connection = node
+        ? node instanceof DocDBDocumentsTreeItem
+            ? createNoSqlQueryConnection(node.parent)
+            : createNoSqlQueryConnection(node)
+        : undefined;
 
     QueryEditorTab.render(connection);
 };

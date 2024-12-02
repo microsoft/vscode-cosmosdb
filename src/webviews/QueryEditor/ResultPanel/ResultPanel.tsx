e4cc5621be71cdc1ffe2ce8e9688895337ec87f0
Dmitrii Shilov <dshilov@microsoft.com>
2024-12-02 18:57:00 +0100

    fix: Query Editor toolbar folds to menu to fit the window size

--- src/webviews/QueryEditor/ResultPanel/ResultPanel.tsx
+++ src/webviews/QueryEditor/ResultPanel/ResultPanel.tsx
@@ -22,17 +22,41 @@ const useStyles = makeStyles({
     },
     tabs: {
         flexGrow: 1,
+        /**
+         * Flex should know basis size to calculate grow and shrink.
+         * This value is used to calculate the initial size of the tabs.
+         * This is the sum of the width of both tabs: 60px + 60px
+         */
         flexBasis: '120px',
+        /**
+         * To prevent tabs from shrinking, we set flexBasis to 120px.
+         * This is the sum of the width of both tabs: 60px + 60px
+         */
         minWidth: '120px',
     },
     tabToolbar: {
+        /**
+         * Flex should know basis size to calculate grow and shrink.
+         * This value is used to calculate the initial size of the toolbar.
+         * This is the width of the toolbar:
+         * 4 buttons * 32px + 36px divider + 100px for the combobox + 8px padding (272px)
+         */
         flexBasis: '280px',
         '& [role="toolbar"]': {
             justifyContent: 'flex-end',
         },
     },
     panelToolbar: {
+        /**
+         * Allow the toolbar to shrink to 0 if there is not enough space.
+         */
         minWidth: '0',
+        /**
+         * Flex should know basis size to calculate grow and shrink.
+         * This value is used to calculate the initial size of the toolbar.
+         * This is the width of the toolbar:
+         * 6 buttons * 32px + 3 dividers * 24px + 100px for the combobox + 100px for status bar + 8px padding (472px)
+         */
         flexBasis: '480px',
     },
     tabContainer: {
